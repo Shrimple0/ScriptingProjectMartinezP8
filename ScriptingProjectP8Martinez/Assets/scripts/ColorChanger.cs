d27339d1b1a6b98c96d443c5e6732959b024d627
Shrimple0 <756116@student.dusd.net>
2024-09-25 15:17:22 -0700

    complete script assignment
    scene created completed scripting assignment 1 which is ball w/ 9 colours

---
+++ ScriptingProjectP8Martinez/Assets/scripts/ColorChanger.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GetComponent<Renderer>().material.color = Color.clear;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
