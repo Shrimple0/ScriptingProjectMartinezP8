67622facf4a15fa645e1433c111053eecd8a6734
Shrimple0 <756116@student.dusd.net>
2024-11-07 15:24:01 -0800

    awake and start done

--- ScriptingProjectP8Martinez/Assets/scripts/ScopeAndAccessModifiers.cs
+++ ScriptingProjectP8Martinez/Assets/scripts/ScopeAndAccessModifiers.cs
@@ -2,18 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScopeAndAccessModifiers : MonoBehaviour;
-public class AnotherClass
+public class ScopeAndAccessModifiers : MonoBehaviour
 {
-    public int apples;
-    public int bananas;
-
-
-    private int stapler;
-        private int stellotapep;
-    public void FruitMachine(int a, int b)
-    // Start is called before the first frame update
-        public int alpha = 5;
+    public int alpha = 5;
 
 
     private int beta = 0;
@@ -21,33 +12,25 @@ public class AnotherClass
 
 
     private AnotherClass myOtherClass;
-
-
+    // Start is called before the first frame update
     void Start()
     {
         alpha = 29;
 
         myOtherClass = new AnotherClass();
         myOtherClass.FruitMachine(alpha, myOtherClass.apples);
-        int answer;
-        answer = a + b;
-        Debug.Log("Fruit total: " + answer);
     }
+
 
     void Example(int pens, int crayons)
     {
         int answer;
         answer = pens * crayons * alpha;
         Debug.Log(answer);
-        private void OfficeSort(int a, int b)
     }
-
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Alpha is set to: " + alpha);
-        int answer;
-        answer = a + b;
-        Debug.Log("Office Supplies total: " + answer);
     }
 }
